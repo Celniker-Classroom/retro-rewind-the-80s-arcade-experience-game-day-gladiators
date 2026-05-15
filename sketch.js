c99ddc405f5bde3a8c4ac9fb336f2eae81bf7a9a
Skywing608 <VenableN0879@my.sduhsd.net>
2026-05-15 13:52:03 -0700

    Testing

--- sketch.js
+++ sketch.js
@@ -1,15 +1,11 @@
-// await Canvas();
+await Canvas();
   
-// let tank = new Sprite();
-// tank.img = 'Images/Tank.png';
+let tank = new Sprite();
+//tank.img = 'Images/Tank.png';
   
-// crosshair = new Sprite();
-// crosshair.img = 'Images/Crosshair.png';
-// crosshair.follow(mouse);
-
-await Canvas(80, 128);
-world.gravity.y = 10;
+crosshair = new Sprite(20, 20, 20, 20);
+crosshair.img = 'images/Crosshair.png';
+//crosshair.follow(mouse);
 
 let ball = new Sprite(0, -50, 50);
 
-let floor = new Sprite(0, 50, 80, 5, STA);
